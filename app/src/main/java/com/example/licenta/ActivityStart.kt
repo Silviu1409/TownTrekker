7dbeedcb986b112283d51c28bb5ed439796e30a4
Avian Silviu-Gabriel <silviu.avian@gmail.com>
2023-03-02 20:42:05 +0200

    check that Cloud Firestore has been configured successfully

--- app/src/main/java/com/example/licenta/ActivityStart.kt
+++ app/src/main/java/com/example/licenta/ActivityStart.kt
@@ -2,11 +2,14 @@ package com.example.licenta
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.licenta.databinding.ActivityStartBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ActivityStart : AppCompatActivity() {
     private lateinit var binding: ActivityStartBinding
+    private val tag = "test"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +18,20 @@ class ActivityStart : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.AuthButonLogare.setOnClickListener {
+
+            val db = FirebaseFirestore.getInstance()
+
+            db.collection("test")
+                .get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+                        Log.d(tag, "${document.id} => ${document.data}")
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(tag, "Error getting documents.", exception)
+                }
+
             val intent = Intent(this, ActivityMain::class.java)
 
             startActivity(intent)
